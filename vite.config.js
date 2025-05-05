741ed34d645ded2ef5ca10ce2e9dd5aff5476b33
Yathish1000 <yathishamarakonda@gmail.com>
2025-05-05 13:10:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './', // Add this line
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            // This splits vendor libraries into separate chunks
-            return id.toString().split('node_modules/')[1].split('/')[0];
-          }
-        }
-      }
-    }
-  }
-});
+})
